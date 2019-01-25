a3d2c50de2307142f16cd7a689967f3b1c8d6613
Gilbert Schwaab <gilbert@convey.de>
2019-01-25 18:15:05 +0100

    Next try

--- src/android/BroadcastIntentPlugin.java
+++ src/android/BroadcastIntentPlugin.java
@@ -213,7 +213,7 @@ public class BroadcastIntentPlugin extends CordovaPlugin {
 			byte[] readBytes = initiatingIntent.getByteArrayExtra("com.symbol.datawedge.data_raw");
 			if (null == readBytes)
 			{
-				 List<byte[]> decodeData = (List<byte[]>)initiatingIntent.getStringExtra("com.motorolasolutions.emdk.datawedge.decode_data");
+				 List<byte[]> decodeData = (List<byte[]>)initiatingIntent.getByteArrayExtra("com.motorolasolutions.emdk.datawedge.decode_data");
 				 if (null != decodeData) {
 				 	readBytes = decodeData.get(0);
 				 }
